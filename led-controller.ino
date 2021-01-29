363c7ae87bc3fdadbfbbeb22fb577574e6499ed3
Janki <janek935@gmail.com>
2021-01-29 17:39:28 +0100

    rozbudowa o mozliwosc dodawania roznych trybow dzialania

--- led-controller.ino
+++ led-controller.ino
@@ -3,7 +3,7 @@
 
 #define R_PIN 3
 #define G_PIN 6
-#define B_PIN 9
+#define B_PIN 11
 
 #define INVALID_VAL -1
 
@@ -11,71 +11,126 @@ enum MainState
 {
   WAIT_FOR_START_FLAG_1,
   WAIT_FOR_START_FLAG_2,
+  SELECT_MODE,
   READ_R,
   READ_G,
   READ_B,
   WAIT_FOR_END_FLAG,
 };
 
-int state = WAIT_FOR_START_FLAG_1;
+enum Mode
+{
+  MAIN,
+  SET_RGB_VALUES,
+};
+
+int currentMode = MAIN;
+int currentState = WAIT_FOR_START_FLAG_1;
 int readVal = 0;
 int r, g, b;
 
-void setup() 
+void decideMainMode ();
+
+void setup()
 {
+  analogWrite (R_PIN, 255);
+  analogWrite (G_PIN, 255);
   analogWrite (B_PIN, 255);
-  state = WAIT_FOR_START_FLAG_1;
+  currentState = WAIT_FOR_START_FLAG_1;
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   Serial.begin(9600); // Default communication rate of the Bluetooth module
 }
 
-void decideCurrentState ()
+void tmp ()
 {
-  //Serial.println (state);
-  switch (state)
+  switch (currentMode)
   {
-    case WAIT_FOR_START_FLAG_1:
-      if (readVal == (char) FLAG_VAL)
-      {
-        state = WAIT_FOR_START_FLAG_2;
-      }
-      else
-      {
-        state = WAIT_FOR_START_FLAG_1;
-      }
+    case MAIN:
+
+      decideMainMode ();
+    
     break;
 
-    case WAIT_FOR_START_FLAG_2:
-      if (readVal == (char) FLAG_VAL)
-      {
-        state = READ_R;
-      }
-      else
-      {
-        state = WAIT_FOR_START_FLAG_1;
-      }
+    case SET_RGB_VALUES:
+    
+      decideSetRGBValuesMode ();
+      
     break;
+  }
+}
 
+void decideSetRGBValuesMode ()
+{
+   switch (currentState)
+  {
     case READ_R:
+
       r = readVal;
-      state = READ_G;
-    break;
+      currentState = READ_G;
+
+      break;
 
     case READ_G:
+
       g = readVal;
-      state = READ_B;
-    break;
+      currentState = READ_B;
+
+      break;
 
     case READ_B:
-      b = readVal;
-      state = WAIT_FOR_END_FLAG;
-    break;
 
-    case WAIT_FOR_END_FLAG:
+      b = readVal;
+      currentState = WAIT_FOR_END_FLAG;
+      currentMode = MAIN;
+
+      break;
+  }
+}
+
+void decideMainMode ()
+{
+  switch (currentState)
+  {
+    case WAIT_FOR_START_FLAG_1:
+
       if (readVal == (char) FLAG_VAL)
       {
-        state = WAIT_FOR_START_FLAG_1;
+        currentState = WAIT_FOR_START_FLAG_2;
+      }
+      else
+      {
+        currentState = WAIT_FOR_START_FLAG_1;
+      }
+
+      break;
+
+    case WAIT_FOR_START_FLAG_2:
+
+      if (readVal == (char) FLAG_VAL)
+      {
+        currentState = SELECT_MODE;
+      }
+      else
+      {
+        currentState = WAIT_FOR_START_FLAG_1;
+      }
+
+      break;
+
+    case SELECT_MODE:
+
+      //TODO
+      currentState = READ_R;
+      currentMode = SET_RGB_VALUES;
+
+      break;
+
+    case WAIT_FOR_END_FLAG:
+
+      if (readVal == (char) FLAG_VAL)
+      {
+        currentState = WAIT_FOR_START_FLAG_1;
         Serial.println ("SAVE RGB");
         Serial.print ("R: "); Serial.println (r);
         Serial.print ("G: "); Serial.println (g);
@@ -86,15 +141,96 @@ void decideCurrentState ()
       }
       else
       {
-        state = WAIT_FOR_START_FLAG_1;
+        currentState = WAIT_FOR_START_FLAG_1;
       }
-    break;
+
+      break;
+  }
+}
+
+void decideCurrentState ()
+{
+  switch (currentState)
+  {
+    case WAIT_FOR_START_FLAG_1:
+
+      if (readVal == (char) FLAG_VAL)
+      {
+        currentState = WAIT_FOR_START_FLAG_2;
+      }
+      else
+      {
+        currentState = WAIT_FOR_START_FLAG_1;
+      }
+
+      break;
+
+    case WAIT_FOR_START_FLAG_2:
+
+      if (readVal == (char) FLAG_VAL)
+      {
+        currentState = SELECT_MODE;
+      }
+      else
+      {
+        currentState = WAIT_FOR_START_FLAG_1;
+      }
+
+      break;
+
+    case SELECT_MODE:
+
+      //TODO
+      currentState = READ_R;
+
+      break;
+
+    case READ_R:
+
+      r = readVal;
+      currentState = READ_G;
+
+      break;
+
+    case READ_G:
+
+      g = readVal;
+      currentState = READ_B;
+
+      break;
+
+    case READ_B:
+
+      b = readVal;
+      currentState = WAIT_FOR_END_FLAG;
+
+      break;
+
+    case WAIT_FOR_END_FLAG:
+
+      if (readVal == (char) FLAG_VAL)
+      {
+        currentState = WAIT_FOR_START_FLAG_1;
+        Serial.println ("SAVE RGB");
+        Serial.print ("R: "); Serial.println (r);
+        Serial.print ("G: "); Serial.println (g);
+        Serial.print ("B: "); Serial.println (b);
+        analogWrite (R_PIN, r);
+        analogWrite (G_PIN, g);
+        analogWrite (B_PIN, b);
+      }
+      else
+      {
+        currentState = WAIT_FOR_START_FLAG_1;
+      }
+
+      break;
   }
 }
 
 void readNewValue ()
 {
-  if(Serial.available() > 0)
+  if (Serial.available() > 0)
   { // Checks whether data is comming from the serial port
     readVal = Serial.read(); // Reads the data from the serial port
   }
@@ -104,10 +240,10 @@ void readNewValue ()
   }
 }
 
-void loop() 
+void loop()
 {
   readNewValue ();
- 
+
   if (readVal != INVALID_VAL)
   {
     decideCurrentState ();
